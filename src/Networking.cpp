00e8bace704386979ea6f7cc10cb29b0c154033f
Chicken-Bones <mehvids@gmail.com>
2015-08-07 16:10:06 +1000

    Fix crc calculation to start at 0 and include header

--- src/Networking.cpp
+++ src/Networking.cpp
@@ -36,7 +36,7 @@ int serialReader::checkRadio() {
 	unsigned long ts;
 	ts = micros();
 	isFlooded = 0;
-	uint8_t crc = 0xFF;
+	uint8_t crc = 0;
 	uint8_t *p = (uint8_t *)&radioMessage;
 	
 	//Check for bytes
